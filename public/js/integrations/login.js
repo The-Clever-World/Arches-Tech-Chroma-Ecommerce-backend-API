c2f4b93855c33ce4b6c058e70f3d167534eff429
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-08-22 15:51:38 +0530

    [FIX] fix the issue with Auth

--- public/js/integrations/login.js
+++ public/js/integrations/login.js
@@ -1,28 +1,31 @@
-const email = document.querySelector("#loginEmail");
-const password = document.querySelector("#loginPassword");
+
 const loginButton = document.querySelector('#login')
+
 
 loginButton.addEventListener("click", function (e) {
 
-    fetch(`http://localhost:5000/user/login`, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }), // body data type must match "Content-Type" header
-    }).then((response) =>{
-      console.log(response);
-      window.localStorage.setItem('userDetails', response);
-      //document.location.reload();
-    });
+  const email = document.querySelector("#loginEmail").value;
+  const password = document.querySelector("#loginPass").value;
+
+  console.log(password);
+  console.log(email);
+
+  fetch(`http://localhost:5000/user/login`, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    })
+  }).then((response) => {
+    return response.json();
+  }).then(res => {
+    console.log(res);
+    window.localStorage.setItem('userDetails', JSON.stringify(res));
+    document.location.reload();
   });
+});
 
--- public/js/integrations/register.js
+++ public/js/integrations/register.js
@@ -1,29 +1,25 @@
-const userName = document.querySelector("#username");
-const userEmail = document.querySelector("#email");
-const userPassword = document.querySelector("#password");
+
 const registerButton = document.querySelector('#register')
 
 registerButton.addEventListener("click", function (e) {
 
-    fetch(`http://localhost:5000/user/register`, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({
-        name: userName,
-        email: userEmail,
-        password: userPassword,
-      }) // body data type must match "Content-Type" header
-    }).then((response) =>{
-        console.log(response);
-        document.location.reload();
-      });
+  const userName = document.querySelector("#username").value;
+  const userEmail = document.querySelector("#email").value;
+  const userPassword = document.querySelector("#password").value;
+
+  fetch(`http://localhost:5000/user/register`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+      password: userPassword,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    console.log(response);
+    document.location.reload()
   });
+});
 

d123277f40bd28dd74e8ff4ed54e7a8ae81cef75
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-08-28 17:57:12 +0530

    [NEW] added hosted APIs

--- public/js/integrations/addProductDashboard.js
+++ public/js/integrations/addProductDashboard.js
@@ -2,88 +2,88 @@ const addCategory = document.querySelector("#categorySubmit");
 const addProductForm = document.querySelector("#addProductForm");
 
 if (addCategory) {
-  addCategory.addEventListener("click", (e) => {
-    console.log(addCategory);
+    addCategory.addEventListener("click", (e) => {
+        console.log(addCategory);
 
-    // category name & image
-    const categoryName = document.querySelector("#categoryName").value;
-    const categoryImage = document.querySelector("#categoryImage").value;
+        // category name & image
+        const categoryName = document.querySelector("#categoryName").value;
+        const categoryImage = document.querySelector("#categoryImage").value;
 
-    console.log(categoryName);
-    console.log(categoryImage);
+        console.log(categoryName);
+        console.log(categoryImage);
 
-    fetch(`https://chroma-commerce.herokuapp.com/admin/categories/add`, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        categoryName: categoryName,
-        categoryImage: categoryImage,
-      }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        console.log(res);
-        alert(res.name + " category added successfully");
-        document.location.reload();
-      });
-  });
+        fetch(`https://chroma-commerce.herokuapp.com/admin/categories/add`, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                categoryName: categoryName,
+                categoryImage: categoryImage,
+            }),
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((res) => {
+                console.log(res);
+                alert(res.name + " category added successfully");
+                document.location.reload();
+            });
+    });
 }
 
 addProductForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const value = Object.fromEntries(formData.entries());
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const value = Object.fromEntries(formData.entries());
 
-  // construct object to be sent to DB
+    // construct object to be sent to DB
 
-  //filter out null values and empty strings
-  //[!warning] mutates object directly
-  Object.keys(value).forEach(
-    (k) => value[k] == (null || "") && delete value[k]
-  );
+    //filter out null values and empty strings
+    //[!warning] mutates object directly
+    Object.keys(value).forEach(
+        (k) => value[k] == (null || "") && delete value[k]
+    );
 
-  // rating and numReviews fixed manually
-  const product = {
-    categoryName: value.category,
-    productName: value.name,
-    productPrice: value.price,
-    productImages: [value.Image1, value.Image2, value.Image3, value.Image4],
-    productCountInStock: value.stock,
-    productRating: 4.5,
-    productNumReviews: 14,
-    productKeyFeatures: [
-      value.keyFeature1,
-      value.keyFeature2,
-      value.keyFeature3,
-      value.keyFeature4,
-    ],
-    productSpecifications: value.specification,
-    productHighlights: value.Highlight,
-    productOverview: value.overview,
-  };
+    // rating and numReviews fixed manually
+    const product = {
+        categoryName: value.category,
+        productName: value.name,
+        productPrice: value.price,
+        productImages: [value.Image1, value.Image2, value.Image3, value.Image4],
+        productCountInStock: value.stock,
+        productRating: 4.5,
+        productNumReviews: 14,
+        productKeyFeatures: [
+            value.keyFeature1,
+            value.keyFeature2,
+            value.keyFeature3,
+            value.keyFeature4,
+        ],
+        productSpecifications: value.specification,
+        productHighlights: value.Highlight,
+        productOverview: value.overview,
+    };
 
-  //   console.log(product);
+    //   console.log(product);
 
-  //send data to server
-  fetch(`http://localhost:5000/admin/product/add`, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(product),
-  })
-    .then((response) => {
-      return response.json();
+    //send data to server
+    fetch(`https://chroma-commerce.herokuapp.com/admin/product/add`, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
     })
-    .then((res) => {
-      console.log(res);
-      alert("Product Added successfully");
-      document.location.reload();
-    });
+        .then((response) => {
+            return response.json();
+        })
+        .then((res) => {
+            console.log(res);
+            alert("Product Added successfully");
+            document.location.reload();
+        });
 });
